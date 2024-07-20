6b621864b45ce7ba43bc815e3218ba7d6027d822
urstrulynishkarsh <206301010@gkv.ac.in>
2024-07-20 09:51:31 +0530

    ALLDone

---
+++ src/components/resume-card.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardHeader } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+import { ChevronRightIcon, School } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+interface ResumeCardProps {
+  logoUrl: string;
+  altText: string;
+  title: string;
+  subtitle?: string;
+  href?: string;
+  badges?: readonly string[];
+  period: string;
+  description?: string;
+}
+export const ResumeCard = ({
+  logoUrl,
+  altText,
+  title,
+  subtitle,
+  href,
+  badges,
+  period,
+  description,
+}: ResumeCardProps) => {
+  const [isExpanded, setIsExpanded] = React.useState(false);
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (description) {
+      e.preventDefault();
+      setIsExpanded(!isExpanded);
+    }
+  };
+
+  return (
+    <Link
+      href={href || "#"}
+      className="block cursor-pointer"
+      onClick={handleClick}
+    >
+      <Card className="flex">
+        <div className="flex-none">
+          <Avatar className="border size-12 m-auto bg-muted-background dark:bg-foreground">
+            <AvatarImage
+              src={logoUrl}
+              alt={altText}
+              className="object-contain"
+            />
+            <AvatarFallback>{altText[0]}</AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="flex-grow ml-4 items-center flex-col group">
+          <CardHeader>
+            <div className="flex items-center justify-between gap-x-2 text-base">
+              <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
+                {title}
+                {badges && (
+                  <span className="inline-flex gap-x-1">
+                    {badges.map((badge, index) => (
+                      <Badge
+                        variant="secondary"
+                        className="align-middle text-xs"
+                        key={index}
+                      >
+                        {badge}
+                      </Badge>
+                    ))}
+                  </span>
+                )}
+                <ChevronRightIcon
+                  className={cn(
+                    "size-4 translate-x-0 transform opacity-0 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:opacity-100",
+                    isExpanded ? "rotate-90" : "rotate-0"
+                  )}
+                />
+              </h3>
+              <div className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right">
+                {period}
+              </div>
+            </div>
+            {subtitle && <div className="font-sans text-xs">{subtitle}</div>}
+            {/* {
+              title==="Gurukul Kangri Deemed to be University, Haridwar (GFTI)" && <div className="font-sans text-xs">CGPA 9.05</div>
+            } */}
+            {/* {
+              title==="Morning Star Children’s Senior Secondary Academy" && <div className="font-sans text-xs">78% & 86.2%</div>
+            } */}
+          </CardHeader>
+          {description && (
+            <motion.div
+              initial={{ opacity: 0, height: 0 }}
+              animate={{
+                opacity: isExpanded ? 1 : 0,
+
+                height: isExpanded ? "auto" : 0,
+              }}
+              transition={{
+                duration: 0.7,
+                ease: [0.16, 1, 0.3, 1],
+              }}
+              className="mt-2 text-xs sm:text-sm"
+            >
+              <ul>
+                <li>•Played a pivotal role in the GUB Project, a government domain initiative in collaboration with the Kolkata government, aimed at enhancing user management and data monitoring.</li>
+
+                <li>•Developed a dynamic user list, which includes real-time statistics on the number of active users, offering valuable insights into user engagement. The Frontend of this Project in ReactTS.</li>
+<li>• Demonstrated expertise in Redux store management for efficient data retrieval from the backend, ensuring seamless integration of user panel with real-time data updates.</li>
+<li>• Contributed to the development and maintenance of the Srishtishree E-Commerce Project, a dynamic platform that involved the creation of user-friendly e-commerce solutions.</li>
+<li>• After website development, created comprehensive website documentation and a workflow chart to enhance project transparency and facilitate team collaboration.</li>
+
+              </ul>
+            </motion.div>
+          )}
+        </div>
+      </Card>
+    </Link>
+  );
+};
